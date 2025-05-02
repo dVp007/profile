ab4170d03fd6a3911a9886727018948569ce5176
Dharmil Parekh <dharmilp03@gmail.com>
2025-05-02 17:16:15 +0100

    feat: add deployment step

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/profile/" : "",
+  basePath: isProd ? "/profile" : "",
+  output: "export",
+};
+
+export default nextConfig;
